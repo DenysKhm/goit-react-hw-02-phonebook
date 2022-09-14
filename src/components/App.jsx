e1791758216e1865459df5be38f0a703195ad163
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-09-14 21:22:41 +0300

    render contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import ContactForm from './ContactForm/ContactForm';
+
+export default class App extends React.Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  render () {
+    return (
+    <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+
+  <h2>Contacts</h2>
+  {/* <Filter/>
+  <ContactList/>  */}
+</div>
+)}
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import css from './ContactForm.module.css';
+// import { nanoid } from 'nanoid'
+
+export default class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleOnChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <>
+        <form className={css.contactForm} onSubmit={this.handleSubmit}>
+          <label className={css.contactLabel}>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              className={css.contactInput}
+              onChange={this.handleOnChange}
+              value={name}
+            />
+          </label>
+          <label className={css.contactLabel}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              className={css.contactInput}
+              onChange={this.handleOnChange}
+              value={number}
+            />
+          </label>
+          <button type="submit" className={css.btn}>
+            Add contact
+          </button>
+        </form>
+      </>
+    );
+  }
+}
